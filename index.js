7910193071d5ed538367da4e31f44c5782109022
Jahongir-web <jahongirmh@gmail.com>
2023-04-08 17:58:39 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const fileUpload = require('express-fileupload');
+
+dotenv.config()
+const app = express();
+const PORT = process.env.PORT || 4001;
+
+// to save files for public
+app.use(express.static('src/files'))
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(fileUpload());
+app.use(cors());
+
+// Routes
+
+app.get('/', (req, res) => {
+  res.send('ok')
+})
+
+const runServer = async () => {
+  try {
+    await mongoose.connect('mongodb://localhost:27017/libraryApp', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    app.listen(PORT, ()=> console.log(`Server started listen on port: ${PORT}`))
+  } catch (error) {
+    console.log(error.message);
+    process.exit(1)
+  }
+}
+
+runServer()
+
+
